6cb63c079bc1aa7a3c156caef02022abacb6d727
Sourcery AI <>
2023-12-14 15:26:47 +0000

    'Refactored by Sourcery'

--- QR-Code-Generator.py
+++ QR-Code-Generator.py
@@ -2,7 +2,7 @@ import qrcode
 
 profile = input("Enter your profile: ")
 
-data = 'https://www.linkedin.com/in/' + profile
+data = f'https://www.linkedin.com/in/{profile}'
 
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 qr.add_data(data)
@@ -13,4 +13,4 @@ destination = input("Enter the destination directory to save the QR code image: 
 
 filename = input("Enter the filename for the QR code image (including the extension, like, myqrcode.png): ")
 
-filepath = destination + '/' + filename
+filepath = f'{destination}/{filename}'
